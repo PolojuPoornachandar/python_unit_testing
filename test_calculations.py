52a27e9aff559e36708bb5c53747b6a058d97dbf
Polo <chandu@Polos-MacBook-Air.local>
2025-03-24 14:06:40 -0700

    modified

--- test_calculations.py
+++ test_calculations.py
@@ -1,41 +1,41 @@
 import unittest
-import calc
+import calculations
 
-class TestCalc(unittest.TestCase):
+class Test_Calculations(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(calc.add(10, 5), 15)
-        self.assertEqual(calc.add(100, 1), 101)
-        self.assertEqual(calc.add(-88, 88), 0)
-        self.assertEqual(calc.add(2, 2), 4)
+        self.assertEqual(calculatios.add(10, 5), 15)
+        self.assertEqual(calculations.add(100, 1), 101)
+        self.assertEqual(calculations.add(-88, 88), 0)
+        self.assertEqual(calculations.add(2, 2), 4)
 
     def test_subtract(self):
-        self.assertEqual(calc.subtract(10, 5), 5)
-        self.assertEqual(calc.subtract(100, 1), 99)
-        self.assertEqual(calc.subtract(-88, 88), -176)
-        self.assertEqual(calc.subtract(2, 2), 0)
+        self.assertEqual(calculations.subtract(10, 5), 5)
+        self.assertEqual(calculations.subtract(100, 1), 99)
+        self.assertEqual(calculations.subtract(-88, 88), -176)
+        self.assertEqual(calculations.subtract(2, 2), 0)
 
     def test_divide(self):
-        self.assertEqual(calc.divide(10, 5), 2)
-        self.assertEqual(calc.divide(100, 1), 100)
-        self.assertEqual(calc.divide(-88, 88), -1)
-        self.assertEqual(calc.divide(2, 2), 1)
+        self.assertEqual(calculations.divide(10, 5), 2)
+        self.assertEqual(calculations.divide(100, 1), 100)
+        self.assertEqual(calculations.divide(-88, 88), -1)
+        self.assertEqual(calculations.divide(2, 2), 1)
 
-        # self.assertRaises(ValueError, calc.divide, 10, 0)
+        # self.assertRaises(ValueError, calculations.divide, 10, 0)
 
         with self.assertRaises(ValueError):
             n = 10
             l = 0
-            calc.divide(n, l)
+            calculations.divide(n, l)
 
         with self.assertRaises(ValueError):
-            calc.divide(99999, 0)
+            calculations.divide(99999, 0)
 
     def test_multiply(self):
-        self.assertEqual(calc.multiply(10, 5), 50)
-        self.assertEqual(calc.multiply(100, 1), 100)
-        self.assertEqual(calc.multiply(-88, 88), -7744)
-        self.assertEqual(calc.multiply(2, 2), 4)
+        self.assertEqual(calculations.multiply(10, 5), 50)
+        self.assertEqual(calculations.multiply(100, 1), 100)
+        self.assertEqual(calculations.multiply(-88, 88), -7744)
+        self.assertEqual(calculations.multiply(2, 2), 4)
     
 
 if __name__ == '__main__':
